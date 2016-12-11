3dc1d9a7f7a4316081c2a8d4a36b7cb640e8e320
Rafael Escobar <rescobar@tuta.io>
2016-12-10 22:59:47 -0200

    Add methods for file information.

--- ipe.go
+++ ipe.go
@@ -1,29 +1,45 @@
 package ipe
 
-import (
-	"errors"
-	"os"
-)
+import "os"
 
-var errIsNotDir = errors.New("entry is not a dir")
-
-type Entry struct {
-	absPath  string
+type File struct {
 	fileInfo os.FileInfo
 }
 
-func (e Entry) Name() string {
-	return e.fileInfo.Name()
+type RecurFile struct {
+	File
+	Children []RecurFile
 }
 
-func (e Entry) Size() int64 {
-	return e.fileInfo.Size()
-}
+func (f File) Name() string { return f.fileInfo.Name() }
+func (f File) Size() int64  { return f.fileInfo.Size() }
 
-func (e Entry) IsDir() bool {
-	return e.fileInfo.IsDir()
-}
+func (f File) IsDir() bool        { return f.fileInfo.Mode()&os.ModeDir != 0 }
+func (f File) IsAppend() bool     { return f.fileInfo.Mode()&os.ModeAppend != 0 }
+func (f File) IsExclusive() bool  { return f.fileInfo.Mode()&os.ModeExclusive != 0 }
+func (f File) IsTemporary() bool  { return f.fileInfo.Mode()&os.ModeTemporary != 0 }
+func (f File) IsSymlink() bool    { return f.fileInfo.Mode()&os.ModeSymlink != 0 }
+func (f File) IsDevice() bool     { return f.fileInfo.Mode()&os.ModeDevice != 0 }
+func (f File) IsNamedPipe() bool  { return f.fileInfo.Mode()&os.ModeNamedPipe != 0 }
+func (f File) IsSocket() bool     { return f.fileInfo.Mode()&os.ModeSocket != 0 }
+func (f File) IsSetuid() bool     { return f.fileInfo.Mode()&os.ModeSetuid != 0 }
+func (f File) IsSetgid() bool     { return f.fileInfo.Mode()&os.ModeSetgid != 0 }
+func (f File) IsCharDevice() bool { return f.fileInfo.Mode()&os.ModeCharDevice != 0 }
+func (f File) IsSticky() bool     { return f.fileInfo.Mode()&os.ModeSticky != 0 }
 
-func (e Entry) AbsPath() string {
-	return e.absPath
+func Open(path string) ([]File, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	list, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		return nil, err
+	}
+	rlist := make([]File, len(list))
+	for _, file := range list {
+		rlist = append(rlist, File{file})
+	}
+	return rlist, nil
 }
